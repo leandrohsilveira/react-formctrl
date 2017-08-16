dac1081278873d2e36caf1e02cd17384680843fd
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2017-08-16 00:55:05 -0300

    Splitting project bundles

--- webpack.config.js
+++ webpack.config.js
@@ -7,24 +7,21 @@ module.exports = {
     resolve: {
         extensions: [".webpack.js", ".web.js", ".js", ".jsx"],
     },
-    entry:   [
-        // "react-hot-loader/patch", // activate HMR for React
-        // "webpack-dev-server/client?http://localhost:8080",// bundle the client for webpack-dev-server and connect to the provided endpoint
-        // "webpack/hot/only-dev-server", // bundle the client for hot reloading, only- means to only hot reload for successful updates
-        "./app.jsx" // the entry point of our app
-    ],
-    output:  {
-        filename:   "bundle.js", // the output bundle
-        path:       resolve(__dirname, "example/dist"),
-        publicPath: "/" // necessary for HMR to know where to load the hot update chunks
+    entry: {
+        vendor: ['react', 'react-dom'],
+        bundle: './app.jsx',
     },
-
-    context: resolve(__dirname, 'example'),
+    output:  {
+        filename:   "[name].js", // the output bundle
+        path:       resolve(__dirname, "example/dist")
+    },
+// 
+    context: resolve(__dirname, "example"),
     devtool: "inline-source-map",
 
     devServer: {
         hot:         true, // enable HMR on the server
-        contentBase: resolve(__dirname, "dist"), // match the output path
+        contentBase: resolve(__dirname, "example/dist"), // match the output path
         publicPath:  "/" // match the output `publicPath`
     },
 
@@ -41,7 +38,11 @@ module.exports = {
     plugins:     [
         // new StyleLintPlugin(),
         new webpack.HotModuleReplacementPlugin(), // enable HMR globally
-        new webpack.NamedModulesPlugin(), // prints more readable module names in the browser console on HMR updates
+        // new webpack.NamedModulesPlugin(), // prints more readable module names in the browser console on HMR updates
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'react',
+        }),
+        // new webpack.optimize.UglifyJsPlugin(),
         new HtmlWebpackPlugin({
             template: 'index.html',
             filename: 'index.html',
