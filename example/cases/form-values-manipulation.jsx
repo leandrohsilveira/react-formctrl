2cd733a978228a515c5f4fb2fd306bd7587168ae
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2017-09-19 00:10:05 -0300

    Demo code fixed for code highlightinh

--- example/cases/form-values-manipulation.jsx
+++ example/cases/form-values-manipulation.jsx
@@ -26,7 +26,7 @@ function Input({ label, name, value, onChange, onBlur, ctrl: { invalid, errors }
 function InputField({ label, formName, fieldName, type = 'text', required, pattern, className }) {
     return (
         <Field form={formName} name={fieldName} type={type} required={required} pattern={pattern} className={className}>
-            <Input label={label} />
+            <Input label={label}></Input>
         </Field>
     )
 }
@@ -34,9 +34,9 @@ function InputField({ label, formName, fieldName, type = 'text', required, patte
 function SampleForm({ formName, formCtrl, onSubmit }) {
     return (
         <Form name={formName} onSubmit={onSubmit}>
-            <InputField label="Name (required)" formName={formName} fieldName="name" required />
-            <InputField label="E-mail (email and required)" formName={formName} fieldName="email" type="email" required />
-            <InputField label="Age (number and required)" formName={formName} fieldName="age" type="number" required />
+            <InputField label="Name (required)" formName={formName} fieldName="name" required></InputField>
+            <InputField label="E-mail (email and required)" formName={formName} fieldName="email" type="email" required></InputField>
+            <InputField label="Age (number and required)" formName={formName} fieldName="age" type="number" required></InputField>
 
             <div>
                 <button type="button" onClick={() => formCtrl.setFieldValue('age', `${parseInt(Math.random() * 100)}`)}>Set random age</button>
@@ -56,7 +56,7 @@ export function FormValuesManipulationExample(props) {
             <p>This is a more complex example of form controller manipulation to programatically change state. Be careful with setValue method, calling it directly on component update phase may result on stack overflow error!</p>
             <div>
                 <FormControl name={formName}>
-                    <SampleForm onSubmit={handleSubmit} />
+                    <SampleForm onSubmit={handleSubmit}></SampleForm>
                 </FormControl>
             </div>
         </div>
