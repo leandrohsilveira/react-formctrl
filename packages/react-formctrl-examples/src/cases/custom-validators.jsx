43a4648c2a1f2655678c65897784e89d0b5863a7
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2017-11-06 17:50:07 -0200

    #40 Updated some example cases to use decorators

--- packages/react-formctrl-examples/src/cases/custom-validators.jsx
+++ packages/react-formctrl-examples/src/cases/custom-validators.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-import {FormProvider, FormControl, Form, Field} from 'react-formctrl'
+import { FormProvider, Form, controlledForm, controlledField } from 'react-formctrl'
 
-import {CustomValidator} from 'react-formctrl/lib/validator'
+import { CustomValidator } from 'react-formctrl/lib/validator'
 
-import {SubmitValuesPopup} from '../components/submit-values'
+import { SubmitValuesPopup } from '../components/submit-values'
 
-function Input({name, label, type, onChange, onBlur, value, ctrl: {valid, invalid, dirty, errors}}) {
+let Input = ({ name, label, type, onChange, onBlur, value, ctrl: { valid, invalid, dirty, errors } }) => {
     const getInputClasses = () => {
-        if(valid) return 'is-valid'
-        if(dirty && invalid) return 'is-invalid'
+        if (valid) return 'is-valid'
+        if (dirty && invalid) return 'is-invalid'
     }
     return (
         <div className="form-group">
@@ -21,24 +21,32 @@ function Input({name, label, type, onChange, onBlur, value, ctrl: {valid, invali
         </div>
     )
 }
+Input = controlledField()(Input)
 
-function CustomValidatorForm({onSubmit, formCtrl: {formName, invalid, unchanged}}) {
+let CustomValidatorForm = ({ onSubmit, formCtrl: { formName, invalid, unchanged } }) => {
     return (
         <Form name={formName} onSubmit={onSubmit}>
-            <Field form={formName} name="username1" validate={["noadmin"]} required>
-                <Input label="Username 1 (required, not admin)"></Input>
-            </Field>
-
-            <Field form={formName} name="username2" validate= "noadmin" required>
-                <Input label="Username 2 (required, not admin)"></Input>
-            </Field>
-
+            <Input
+                label="Username 1 (required, not admin)"
+                form={formName}
+                name="username1"
+                validate={["noadmin"]}
+                required
+            />
+            <Input
+                label="Username 2 (required, not admin)"
+                form={formName}
+                name="username2"
+                validate="noadmin"
+                required
+            />
             <button type="submit" className="btn btn-primary" disabled={invalid || unchanged}>Submit</button>
             &nbsp;
             <button type="reset" className="btn btn-default" disabled={unchanged}>Reset</button>
         </Form>
     )
 }
+CustomValidatorForm = controlledForm()(CustomValidatorForm)
 
 export function CustomValidatorExample() {
     const handleSubmit = values => SubmitValuesPopup.dispatchShowSubmitValuesPopupEvent("Custom validator form", values)
@@ -46,15 +54,13 @@ export function CustomValidatorExample() {
         <div>
             <h3>Custom validators</h3>
             <p>There is a example of the custom field validation usage, the "Field" component can have a "validate" property to map a custom validation registered on "FieldProvider" component.</p>
-            <FormControl form="customValidatorExampleForm">
-                <CustomValidatorForm onSubmit={handleSubmit}></CustomValidatorForm>
-            </FormControl>
+            <CustomValidatorForm form="customValidatorExampleForm" onSubmit={handleSubmit}></CustomValidatorForm>
         </div>
     )
 }
 
 class NoAdminValidator extends CustomValidator {
-    
+
     constructor() {
         super('noadmin') // This constructor parameter defines the error message key
     }
