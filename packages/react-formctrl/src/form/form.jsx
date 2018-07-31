027eeffa442159a2ab3709337a9f4e5693ebff4b
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2018-07-31 16:00:19 -0300

    #55 using FormConsumer on Form and FormControl components

--- packages/react-formctrl/src/form/form.jsx
+++ packages/react-formctrl/src/form/form.jsx
@@ -1,81 +1,68 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import {FormEventDispatcher} from '../provider/provider'
-import {REACT_FORMCTRL} from '../provider/provider.actions'
-import {ensureStringValue} from '../provider/provider.utils'
+import { ensureStringValue } from '../provider/provider.utils'
+import { FormConsumer } from '../provider/provider';
+import { onSubmitForm, onResetForm, onRegisterForm, onUnregisterForm, onFieldChanged } from './../provider/provider.actions';
 
-export class Form extends React.Component {
-
-    static propTypes = {
-        name: PropTypes.string.isRequired,
-        className: PropTypes.string,
-        onSubmit: PropTypes.func,
-        onReset: PropTypes.func
-    }
+class WrappedForm extends React.Component {
 
     constructor(props) {
         super(props)
-        this.state = {
-            ref: `${Math.random() * 999999999999999999}-${new Date().getTime()}`
-        }
         this.handleSubmit = this.handleSubmit.bind(this)
         this.handleReset = this.handleReset.bind(this)
         this.handleFormSubmitForward = this.handleFormSubmitForward.bind(this)
         this.handleFormResetForward = this.handleFormResetForward.bind(this)
+        this.dispatch = this.dispatch.bind(this);
+    }
+
+    dispatch(action) {
+        const { name, dispatch } = this.props;
+        return dispatch(action)
+            .then(forms => forms[name]);
     }
 
     handleSubmit(event) {
-        const {name} = this.props
-        const {ref} = this.state
+        const { name } = this.props
         event.preventDefault()
-        FormEventDispatcher.dispatchSubmitForm(name, ref)
+        this.dispatch(onSubmitForm(name))
+            .then(this.handleFormSubmitForward)
     }
 
     handleReset(event) {
-        const {name} = this.props
-        const {ref} = this.state
+        const { name } = this.props
         event.preventDefault()
-        FormEventDispatcher.dispatchResetForm(name, ref, event.type)
+        this.dispatch(onResetForm(name, event.type))
+                .then(this.handleFormResetForward)
     }
-    
+
     componentWillMount() {
-        const {name} = this.props
-        document.addEventListener(`${REACT_FORMCTRL.EVENTS.FORM_SUBMITED}#${name}`, this.handleFormSubmitForward)
-        document.addEventListener(`${REACT_FORMCTRL.EVENTS.FORM_RESETED}#${name}`, this.handleFormResetForward)
-        FormEventDispatcher.dispatchRegisterForm(name)
+        const { name } = this.props
+        this.dispatch(onRegisterForm(name))
     }
-    
-    handleFormSubmitForward(event) {
-        const {onSubmit} = this.props
-        if(typeof onSubmit === 'function') {
-            const {values, formCtrl, formRef} = event.detail
-            if(formRef === null || formRef == this.state.ref) {
-                onSubmit(values, formCtrl)
-            }
+
+    handleFormSubmitForward({ values, formCtrl }) {
+        const { onSubmit } = this.props
+        if (typeof onSubmit === 'function') {
+            onSubmit(values, formCtrl)
         }
     }
-    
-    handleFormResetForward(event) {
-        const {onReset} = this.props
-        if(typeof onReset === 'function') {
-            const {formRef} = event.detail
-            if(formRef === null || formRef == this.state.ref) {
-                onReset()
-            }
+
+    handleFormResetForward() {
+        const { onReset } = this.props
+        if (typeof onReset === 'function') {
+            onReset()
         }
     }
-    
+
     componentWillUnmount() {
-        const {name} = this.props
-        document.removeEventListener(`${REACT_FORMCTRL.EVENTS.FORM_SUBMITED}#${name}`, this.handleFormSubmitForward)
-        document.removeEventListener(`${REACT_FORMCTRL.EVENTS.FORM_RESETED}#${name}`, this.handleFormResetForward)
-        FormEventDispatcher.dispatchUnregisterForm(name)
+        const { name } = this.props
+        this.dispatch(onUnregisterForm(name))
     }
-    
+
     render() {
-        const {handleSubmit, handleReset} = this
-        const {name, children, className} = this.props
+        const { handleSubmit, handleReset } = this
+        const { name, children, className } = this.props
         return (
             <form id={name} name={name} className={className} onSubmit={handleSubmit} onReset={handleReset} noValidate>
                 {children}
@@ -85,29 +72,28 @@ export class Form extends React.Component {
 
 }
 
-export class FormControl extends React.Component {
+const Form = React.forwardRef((props, ref) => (
+    <FormConsumer>
+        {({ dispatch }) => {
+            <WrappedForm
+                {...props}
+                ref={ref}
+                dispatch={dispatch}
+            />
+        }}
+    </FormConsumer>
+))
+Form.propTypes = {
+    name: PropTypes.string.isRequired,
+    className: PropTypes.string,
+    onSubmit: PropTypes.func,
+    onReset: PropTypes.func
+}
 
-    static propTypes = {
-        form: PropTypes.string.isRequired,
-        onChange: PropTypes.func,
-        inject: PropTypes.func,
-    }
+class WrappedFormControl extends React.Component {
 
     constructor(props) {
         super(props)
-        this.state = {
-            valid: true,
-            invalid: false,
-            untouched: true,
-            touched: false,
-            pristine: true,
-            dirty: false,
-            unchanged: true,
-            changed: false,
-            fields: {},
-            values: {},
-            files: {}
-        }
         this.handleFormChanged = this.handleFormChanged.bind(this)
         this.inject = this.inject.bind(this)
         this.setFieldValue = this.setFieldValue.bind(this)
@@ -115,59 +101,70 @@ export class FormControl extends React.Component {
     }
 
     componentWillMount() {
-        const {form, children} = this.props
-        if(Array.isArray(children) && children.length > 1) {
+        const { form, children } = this.props
+        if (Array.isArray(children) && children.length > 1) {
             throw `The FormControl component for "${form}" should have only one child, but has ${children.length}.`
         }
-        document.addEventListener(`${REACT_FORMCTRL.EVENTS.FORM_CHANGED}#${form}`, this.handleFormChanged)
-    }
-    
-    componentWillUnmount() {
-        const {form} = this.props
-        document.removeEventListener(`${REACT_FORMCTRL.EVENTS.FORM_CHANGED}#${form}`, this.handleFormChanged)
     }
 
-    componentDidUpdate() {
-        const {form, onChange} = this.props
-        if(typeof onChange === 'function') {
-            onChange({...this.state, formName: form, setFieldValue: this.setFieldValue, reset: this.reset})
+    componentWillReceiveProps(nextProps) {
+        const { form, onChange, formCtrl } = nextProps
+        if (typeof onChange === 'function') {
+            onChange({ ...formCtrl, formName: form, setFieldValue: this.setFieldValue, reset: this.reset })
         }
-    }
-
-    handleFormChanged(event) {
-        const payload = event.detail
-        const {form} = this.props
-        const {formCtrl} = payload
-        this.setState(formCtrl)
     }
 
     setFieldValue(fieldName, value) {
-        const {form} = this.props
-        const fieldCtrl = this.state.fields[fieldName]
-        if(fieldCtrl) {
-            FormEventDispatcher.dispatchFieldChanged(form, fieldName, ensureStringValue(value, fieldCtrl.props.type))
+        const { form, formCtrl, dispatch } = this.props
+        const fieldCtrl = formCtrl.fields[fieldName]
+        if (fieldCtrl) {
+            dispatch(onFieldChanged(form, fieldName, ensureStringValue(value, fieldCtrl.props.type)))
         } else {
             console.warn(`Field "${fieldName}" not found on form ${form}`)
         }
     }
 
     reset() {
-        const {form} = this.props
-        FormEventDispatcher.dispatchResetForm(form, null, 'reset');
+        const { form, dispatch } = this.props
+        dispatch(onResetForm(form, 'reset'))
     }
 
     inject() {
-        const {inject, form} = this.props
-        const formCtrl = {...this.state, formName: form, setFieldValue: this.setFieldValue, reset: this.reset}
-        if(typeof inject === 'function') {
+        const { inject, form, formCtrl } = this.props
+        const formCtrl = { ...formCtrl, formName: form, setFieldValue: this.setFieldValue, reset: this.reset }
+        if (typeof inject === 'function') {
             return inject(formCtrl)
         }
-        return {formCtrl}
+        return { formCtrl }
     }
 
     render() {
-        const {children} = this.props
-        return React.cloneElement(children, {...children.props, ...this.inject()})
+        const { children } = this.props
+        return React.cloneElement(children, { ...children.props, ...this.inject() })
     }
 
+}
+
+const FormControl = React.createRef((props, ref) => (
+    <FormConsumer>
+        {({ forms, dispatch }) => {
+            <WrappedFormControl
+                {...props}
+                ref={ref}
+                dispatch={dispatch}
+                formCtrl={forms[props.form]}
+            />
+        }}
+    </FormConsumer>
+))
+
+FormControl.propTypes = {
+    form: PropTypes.string.isRequired,
+    onChange: PropTypes.func,
+    inject: PropTypes.func,
+}
+
+export {
+    Form,
+    FormControl
 }
