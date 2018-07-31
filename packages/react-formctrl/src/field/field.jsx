1ce97bd99ee85b976c45ab0653eddb1fa5194343
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2018-07-31 18:04:09 -0300

    #55 using FormConsumer on Field component

--- packages/react-formctrl/src/field/field.jsx
+++ packages/react-formctrl/src/field/field.jsx
@@ -1,60 +1,12 @@
 import React from 'react'
-import PropTypes, { instanceOf } from 'prop-types'
+import PropTypes from 'prop-types'
 
 import { FormEventDispatcher } from '../provider/provider'
-import { REACT_FORMCTRL } from '../provider/provider.actions'
-import { compareFieldProps, formatDate, formatDateTime, ensureStringValue } from '../provider/provider.utils'
+import { REACT_FORMCTRL, onUnregisterField, onFieldChanged } from '../provider/provider.actions'
+import { compareFieldProps, ensureStringValue } from '../provider/provider.utils'
+import { onFieldPropsChanged, onRegisterField, onFieldBlur } from './../provider/provider.actions';
 
-export class Field extends React.Component {
-
-    static propTypes = {
-        name: PropTypes.string.isRequired,
-        form: PropTypes.string.isRequired,
-        className: PropTypes.string,
-        required: PropTypes.bool,
-        pattern: PropTypes.oneOfType([
-            PropTypes.string,
-            PropTypes.instanceOf(RegExp)
-        ]),
-        type: PropTypes.string,
-        integer: PropTypes.bool,
-        inject: PropTypes.func,
-        match: PropTypes.string,
-        min: PropTypes.oneOfType([
-            PropTypes.number,
-            PropTypes.string,
-        ]),
-        max: PropTypes.oneOfType([
-            PropTypes.number,
-            PropTypes.string,
-        ]),
-        minLength: PropTypes.oneOfType([
-            PropTypes.number,
-            PropTypes.string,
-        ]),
-        maxLength: PropTypes.oneOfType([
-            PropTypes.number,
-            PropTypes.string,
-        ]),
-        accept: PropTypes.string,
-        extensions: PropTypes.arrayOf(PropTypes.string),
-        maxSize: PropTypes.oneOfType([
-            PropTypes.number,
-            PropTypes.string
-        ]),
-        validate: PropTypes.oneOfType([
-            PropTypes.string,
-            PropTypes.arrayOf(PropTypes.string)
-        ]),
-        initialValue: PropTypes.oneOfType([
-            PropTypes.string,
-            PropTypes.number,
-            PropTypes.instanceOf(Date)
-        ]),
-        afterChange: PropTypes.func,
-        afterBlur: PropTypes.func,
-        afterReset: PropTypes.func,
-    }
+class FieldWrapper extends React.Component {
 
     constructor(props) {
         super(props)
@@ -62,6 +14,7 @@ export class Field extends React.Component {
         this.onChange = this.onChange.bind(this)
         this.onBlur = this.onBlur.bind(this)
         this.onReset = this.onReset.bind(this)
+        this.handleFormsChange = this.handleFormsChange.bind(this)
         this.handleFieldChangeForward = this.handleFieldChangeForward.bind(this)
         this.handleMatchFieldChangeForward = this.handleMatchFieldChangeForward.bind(this)
         this.handleChange = this.handleChange.bind(this)
@@ -69,8 +22,15 @@ export class Field extends React.Component {
         this.getChildProps = this.getChildProps.bind(this)
         this.inject = this.inject.bind(this)
         this.getFieldProps = this.getFieldProps.bind(this)
+    }
 
-        this.state = {
+    componentWillMount() {
+        const { form, name, children, dispatch } = this.props
+        if (Array.isArray(children) && children.length > 1) {
+            throw `The Field component for "${form}#${name}" should have only one child, but has ${children.length}.`
+        }
+
+        dispatch(onRegisterField(form, name, {
             valid: true,
             invalid: false,
             untouched: true,
@@ -84,38 +44,20 @@ export class Field extends React.Component {
             files: [],
             initialValue: ensureStringValue(props.initialValue || '', props.type),
             props: this.getFieldProps(props)
-        }
-
+        }))
     }
-
-    componentWillMount() {
-        const { form, name, children, match } = this.props
-        if (Array.isArray(children) && children.length > 1) {
-            throw `The Field component for "${form}#${name}" should have only one child, but has ${children.length}.`
-        }
-        document.addEventListener(`${REACT_FORMCTRL.EVENTS.FIELD_CHANGED}#${form}#${name}`, this.handleFieldChangeForward)
-        if(match) {
-            document.addEventListener(`${REACT_FORMCTRL.EVENTS.FIELD_CHANGED}#${form}#${match}`, this.handleMatchFieldChangeForward)
-        }
-
-        FormEventDispatcher.dispatchRegisterField(form, name, this.state)
-    }
-
 
     componentWillReceiveProps(nextProps) {
         const nextFieldProps = this.getFieldProps(nextProps)
-        if(!compareFieldProps(this.state.props, nextFieldProps)) {
-            FormEventDispatcher.dispatchFieldPropsChanged(this.props.form, this.props.name, nextFieldProps)
+        if (!compareFieldProps(this.state.props, nextFieldProps)) {
+            const { dispatch } = nextProps
+            dispatch(onFieldPropsChanged(this.props.form, this.props.name, nextFieldProps))
         }
     }
 
     componentWillUnmount() {
-        const { form, name, match } = this.props
-        document.removeEventListener(`${REACT_FORMCTRL.EVENTS.FIELD_CHANGED}#${form}#${name}`, this.handleFieldChangeForward)
-        if(match) {
-            document.removeEventListener(`${REACT_FORMCTRL.EVENTS.FIELD_CHANGED}#${form}#${match}`, this.handleMatchFieldChangeForward)
-        }
-        FormEventDispatcher.dispatchUnregisterField(form, name)
+        const { form, name, dispatch } = this.props
+        dispatch(onUnregisterField(form, name))
     }
 
     getFieldProps(props) {
@@ -159,76 +101,54 @@ export class Field extends React.Component {
         }
     }
 
-    handleFieldChangeForward(event) {
-        const payload = event.detail
-        const fieldCtrl = payload.fieldCtrl
-        const eventType = payload.eventType
-        this.setState(
-            fieldCtrl, 
-            () => {
-                switch(eventType) {
-                    case 'blur':
-                        this.onBlur(fieldCtrl)
-                        break;
-                    case 'reset':
-                        this.onReset(fieldCtrl)
-                        break;
-                    default:
-                        this.onChange(fieldCtrl)
-                        break;
-                }
-            }
-        )
-    }
-
-    handleMatchFieldChangeForward(event) {
-        const {value, files, valid} = this.state
-        const {form, name} = this.props
-        const payload = event.detail
-        const fieldCtrl = payload.fieldCtrl
-        if(payload.eventType === 'change') {
-            FormEventDispatcher.dispatchFieldChanged(form, name, value, files, 'change')
+    handleFormsChange(forms) {
+        const { form, name } = this.props;
+        const formCtrl = forms[form]
+        if (formCtrl) {
+            return formCtrl.fields[name]
         }
+        return null;
     }
 
     handleChange(event) {
-        const { sync } = this
-        const { form, name } = this.props
+        const { form, name, dispatch } = this.props
         const target = event.target
         const value = target.value
-        const files = target.files 
-        FormEventDispatcher.dispatchFieldChanged(form, name, value, files, event.type)
+        const files = target.files
+
+        dispatch(onFieldChanged(form, name, value, files))
+            .then(this.handleFormsChange)
+            .then(this.onChange)
     }
 
-    handleBlur(event) {
-        if (this.state.untouched) {
-            const { form, name } = this.props
-            FormEventDispatcher.dispatchFieldBlur(form, name, event.type)
-        } else {
-            this.onBlur(this.state)
-        }
+    handleBlur() {
+        const { form, name, dispatch } = this.props
+        dispatch(onFieldBlur(form, name))
+            .then(this.handleFormsChange)
+            .then(this.onBlur)
     }
 
     getChildProps() {
+        const { formCtrl } = this.props
         const props = {}
         props.name = this.props.name
         props.formName = this.props.form
         props.ctrl = {
-            valid: this.state.valid,
-            invalid: this.state.invalid,
-            untouched: this.state.untouched,
-            touched: this.state.touched,
-            pristine: this.state.pristine,
-            dirty: this.state.dirty,
-            unchanged: this.state.unchanged,
-            changed: this.state.changed,
-            errors: this.state.errors,
+            valid: formCtrl.valid,
+            invalid: formCtrl.invalid,
+            untouched: formCtrl.untouched,
+            touched: formCtrl.touched,
+            pristine: formCtrl.pristine,
+            dirty: formCtrl.dirty,
+            unchanged: formCtrl.unchanged,
+            changed: formCtrl.changed,
+            errors: formCtrl.errors,
         }
         props.accept = this.props.accept
         props.extensions = this.props.extensions
         props.maxSize = this.props.maxSize
         props.className = this.props.className
-        props.value = this.state.value
+        props.value = formCtrl.value
         props.required = this.props.required
         props.pattern = this.props.pattern
         props.type = this.props.type || 'text'
@@ -251,4 +171,74 @@ export class Field extends React.Component {
         const { children } = this.props
         return React.cloneElement(children, { ...children.props, ...inject() })
     }
+}
+
+const Field = React.createRef((props, ref) => (
+    <FormConsumer>
+        {({ forms, dispatch }) => {
+            const formCtrl = forms[props.form]
+            if (formCtrl) {
+                const fieldCtrl = formCtrl.fields[props.name]
+                const matchCtrl = formCtrl.fields[props.match]
+                return (
+                    <FieldWrapper
+                        {...props}
+                        ref={ref}
+                        dispatch={dispatch}
+                        fieldCtrl={fieldCtrl}
+                        matchCtrl={matchCtrl}
+                    />
+                )
+            }
+            return null
+        }}
+    </FormConsumer>
+))
+Field.propTypes = {
+    name: PropTypes.string.isRequired,
+    form: PropTypes.string.isRequired,
+    className: PropTypes.string,
+    required: PropTypes.bool,
+    pattern: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.instanceOf(RegExp)
+    ]),
+    type: PropTypes.string,
+    integer: PropTypes.bool,
+    inject: PropTypes.func,
+    match: PropTypes.string,
+    min: PropTypes.oneOfType([
+        PropTypes.number,
+        PropTypes.string,
+    ]),
+    max: PropTypes.oneOfType([
+        PropTypes.number,
+        PropTypes.string,
+    ]),
+    minLength: PropTypes.oneOfType([
+        PropTypes.number,
+        PropTypes.string,
+    ]),
+    maxLength: PropTypes.oneOfType([
+        PropTypes.number,
+        PropTypes.string,
+    ]),
+    accept: PropTypes.string,
+    extensions: PropTypes.arrayOf(PropTypes.string),
+    maxSize: PropTypes.oneOfType([
+        PropTypes.number,
+        PropTypes.string
+    ]),
+    validate: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.arrayOf(PropTypes.string)
+    ]),
+    initialValue: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.number,
+        PropTypes.instanceOf(Date)
+    ]),
+    afterChange: PropTypes.func,
+    afterBlur: PropTypes.func,
+    afterReset: PropTypes.func,
 }
